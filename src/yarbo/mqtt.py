cfe581c8ffcd5f556210c915870aeb654a6671f7
Markus Lassfolk <markus@lassfolk.net>
2026-03-01 13:41:55 +0100

    fix: enforce TLS certificate validation for cloud MQTT connections

--- src/yarbo/mqtt.py
+++ src/yarbo/mqtt.py
@@ -195,10 +195,13 @@ class MqttTransport:
         if self._tls:
             import ssl  # noqa: PLC0415
 
-            self._client.tls_set(
-                ca_certs=self._tls_ca_certs,
-                cert_reqs=ssl.CERT_REQUIRED if self._tls_ca_certs else ssl.CERT_NONE,
-            )
+            if self._tls_ca_certs:
+                self._client.tls_set(
+                    ca_certs=self._tls_ca_certs,
+                    cert_reqs=ssl.CERT_REQUIRED,
+                )
+            else:
+                self._client.tls_set(ssl_context=ssl.create_default_context())
 
         self._client.on_connect = self._on_connect
         self._client.on_disconnect = self._on_disconnect
